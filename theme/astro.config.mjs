1f4b05754c1196ecc4ae5e31ffff369a2abd008a
Andrew <ahmcfadden@gmail.com>
2022-10-11 16:35:44 -0700

    Add Partytownjs
    Added partytownjs to config

--- theme/astro.config.mjs
+++ theme/astro.config.mjs
@@ -5,16 +5,26 @@ import image from "@astrojs/image";
 import lit from "@astrojs/lit";
 
 // https://astro.build/config
+import partytown from "@astrojs/partytown";
+
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://odyssey-theme.littlesticks.dev/',
+  site: 'https://comforting-heliotrope-fc2207.netlify.app/',
   // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
   sitemap: true,
   // Generate sitemap (set to "false" to disable)
-  integrations: [sitemap(), mdx(), image(), lit()], // Add renderers to the config
+  integrations: [sitemap(), mdx(), image(), lit(), partytown({
+      // Adds dataLayer.push as a forwarding-event.
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+    ],
+  // Add renderers to the config
   // This is for the astro-icon package. You can find more about the package here: https://www.npmjs.com/package/astro-icon
   vite: {
     ssr: {
-      external: ["svgo"],
-    },
-  },
+      external: ["svgo"]
+    }
+  }
 });
